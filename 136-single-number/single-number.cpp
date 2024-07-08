ab710f180f84f7e39333a061c5fabaa0655a73d4
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-08 18:54:10 +0530

    Time: 15 ms (51.88%) | Memory: 19.1 MB (98.54%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            ans=ans^nums[i];
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            ans = ans ^ nums[i];
         }
         return ans;
     }
