38aaa0f1c67d2149b3553818a60244a00946430f
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-08 17:18:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (26.98%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -3,9 +3,9 @@ public:
     int minBitFlips(int start, int goal) {
         int cnt = 0;
         int num = start ^ goal;
-        for (int i = 0; i < 31; i++) {
-            if ((num & (1 << i)) != 0)
-                cnt++;
+        while (num) {
+            cnt += num & 1;
+            num = num >> 1;
         }
         return cnt;
     }
