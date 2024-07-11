9c451ded10465c594cb7114cc8f6d9cf7f5e1c11
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-11 14:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (12.83%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,35 +1,25 @@
 class MyStack {
 public:
-queue<int>q1;
-queue<int>q2;
-    MyStack() {
-        
-    }
-    
+    queue<int> q1;
+    MyStack() {}
+
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty())
-        {
-            q2.push(q1.front());
+        q1.push(x);
+        for (int i = 0; i < q1.size() - 1; i++) {
+            q1.push(q1.front());
             q1.pop();
         }
-        swap(q1,q2);
     }
-    
+
     int pop() {
-        int ans=q1.front();
+        int ans = q1.front();
         q1.pop();
         return ans;
-        
     }
-    
-    int top() {
-        return q1.front();
-    }
-    
-    bool empty() {
-       return q1.empty(); 
-    }
+
+    int top() { return q1.front(); }
+
+    bool empty() { return q1.empty(); }
 };
 
 /**
