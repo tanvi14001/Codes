12e9f0086c0c2d533b4dd9a944794a09bffe18c8
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-16 15:35:40 +0530

    Time: 8 ms (25.11%) | Memory: 12.9 MB (6.98%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0)
+                return 0;
+            if (nums[i] < 0)
+                neg += 1;
+        }
+        if (neg % 2 == 1)
+            return -1;
+        return 1;
+    }
+};
