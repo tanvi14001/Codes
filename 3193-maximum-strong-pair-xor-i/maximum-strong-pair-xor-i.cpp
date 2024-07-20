dbfc434ee0432750589cfdef344d7dcc2d06f0bf
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-20 14:54:50 +0530

    Time: 7 ms (68.99%) | Memory: 27.4 MB (13.12%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.cpp
@@ -3,7 +3,7 @@ public:
     int maximumStrongPairXor(vector<int>& nums) {
         int n = nums.size(), ans = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 if (abs(nums[i] - nums[j]) <= min(nums[i], nums[j])) {
                     ans = max(ans, nums[i] ^ nums[j]);
                 }
