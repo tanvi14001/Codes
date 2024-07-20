2f9228935358faeb71ef094ba5c4f1dbd772b1ab
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-20 17:31:50 +0530

    Time: 200 ms (26.36%) | Memory: 67.3 MB (5.62%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-int func(vector<int>& nums, int k)
-{
-    int l=0, r=0, n=nums.size(), cnt=0;
-      map<int,int>mpp;
-      while(r<n)
-      {
-        mpp[nums[r]]++;
-        while(mpp.size()>k)
-        {
-            mpp[nums[l]]--;
-            if(mpp[nums[l]]==0)
-            mpp.erase(nums[l]);
-            l++;
+    int func(vector<int>& nums, int k) {
+        int l = 0, r = 0, n = nums.size(), cnt = 0;
+        map<int, int> mpp;
+        while (r < n) {
+            mpp[nums[r]]++;
+            while (mpp.size() > k) {
+                mpp[nums[l]]--;
+                if (mpp[nums[l]] == 0)
+                    mpp.erase(nums[l]);
+                l++;
+            }
+            cnt += (r - l + 1);
+            r++;
         }
-        cnt+=(r-l+1);
-        r++;
-      }
-      return cnt;
-}
+        return cnt;
+    }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-      return func(nums,k)-func(nums,k-1);
+        return func(nums, k) - func(nums, k - 1);
     }
 };
