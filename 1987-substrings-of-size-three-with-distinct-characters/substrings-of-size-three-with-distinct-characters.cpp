dba1db9fa11dad5331c6044a3da5d9751cb1a801
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-21 11:41:56 +0530

    Time: 3 ms (59.92%) | Memory: 7.7 MB (58.27%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        if(s.length()<3) return 0;
+        int i  = 0;
+        int count = 0;
+        for(int j = 2; j<s.length(); j++){
+            if(s[i] != s[i+1] && s[i] != s[j] && s[i+1]!=s[j]) count++;
+            i = j-1;
+        }
+        return count;
+    }
+};
