3acb7591fa2500086490106e7a4832d78cb72baf
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-21 11:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (58.27%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        if(s.length()<3) return 0;
+        if(s.size()<3) return 0;
         int i  = 0;
         int count = 0;
-        for(int j = 2; j<s.length(); j++){
+        for(int j = 2; j<s.size(); j++){
             if(s[i] != s[i+1] && s[i] != s[j] && s[i+1]!=s[j]) count++;
             i = j-1;
         }
