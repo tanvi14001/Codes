0cef6db80376953ef33bed1debe4d46ecdaaf801
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-25 11:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.37%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.cpp
@@ -1,16 +1,23 @@
+
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
         int n=nums.size();
         int i=0, j=n-1;
-        set<double>ans;
+        vector<double>ans;
         sort(nums.begin(),nums.end());
         while(i<j)
         {
-            ans.insert(double(nums[i]+nums[j])/2);
+            ans.push_back(double(nums[i]+nums[j])/2);
             i++; j--;
         }
-        
-        return ans.size();
+        int cnt=1;
+        sort(ans.begin(),ans.end());
+        for(int i=1;i<ans.size();i++)
+        {
+            if(ans[i]!=ans[i-1]) cnt++;
+        }
+        return cnt;
     }
 };
+    
