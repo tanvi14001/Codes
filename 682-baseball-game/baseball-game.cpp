bd0f42215515aa87e30bbddc760eeedc7fe886c9
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-07-29 15:43:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (29.51%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
-        stack<int>st;
-        int n=operations.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            if(operations[i]=="C" && !st.empty())
-            st.pop();
-            else if(operations[i]=="D")
-            st.push(st.top()*2);
-            else if(operations[i]=="+")
-            {
-                int x=st.top();
+        stack<int> st;
+        int n = operations.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (operations[i] == "C" && !st.empty())
                 st.pop();
-                int y=st.top();
+            else if (operations[i] == "D")
+                st.push(st.top() * 2);
+            else if (operations[i] == "+") {
+                int x = st.top();
+                st.pop();
+                int y = st.top();
                 st.push(x);
-                st.push(x+y);
-            }
-            else st.push(stoi(operations[i]));
+                st.push(x + y);
+            } else
+                st.push(stoi(operations[i]));
         }
-            while(!st.empty())
-            {
-                sum+=st.top();
-                st.pop();
-            }
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
         return sum;
     }
 };
