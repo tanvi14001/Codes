4a5312668e63a4fc17617e8db84763be6dadbbe7
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-08 22:24:53 +0530

    Time: 133 ms (11.12%) | Memory: 74.6 MB (24.49%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> mpp;
-        for (auto i : nums) {
-            mpp[i]++;
-        }
-        for (auto it : mpp) {
-            if (it.second >= 2)
-                return true;
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>=2) return true;
         }
         return false;
     }
