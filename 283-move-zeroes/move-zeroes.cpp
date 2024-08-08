4d79865a36814aad30cd299ec892b8264594d823
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-08 21:50:58 +0530

    Time: 20 ms (26.74%) | Memory: 22 MB (18.95%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = i+1;
+        while (i<nums.size() && j<nums.size()) {
+            if (nums[i] == 0 && nums[j] != 0) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            } else if (nums[i] == 0 && nums[j] == 0) {
+                    j++;
+                }
+             else if (nums[i] != 0 && nums[j] != 0) {
+                j++;
+            } else {
+                i = j;
+                j++;
+            }
+        }
+    }
+};
