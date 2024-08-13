75e22d31c82774ce847069d9658283d4c4208916
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-13 13:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (38.05%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,6 +3,8 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         if (s1 == s2)
             return true;
+        if (s1.size() != s2.size())
+            return false;
         int cnt = 0;
         for (int i = 0; i < s1.size(); i++) {
             if (s1[i] != s2[i])
