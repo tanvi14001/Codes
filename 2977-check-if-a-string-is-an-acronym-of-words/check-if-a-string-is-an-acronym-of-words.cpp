856a59877eaa10741bfac00a7c3a9366a9445354
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-14 14:09:50 +0530

    Time: 24 ms (5.84%) | Memory: 43.4 MB (39.11%) - LeetSync

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        string ans;
+        for(int i=0;i<words.size();i++)
+        {
+            ans+=words[i][0];
+        }
+        return s==ans;
+    }
+};
