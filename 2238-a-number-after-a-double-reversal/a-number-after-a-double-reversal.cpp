a84d61372721c08e4ee40fbb6cfa78ddf9c5676d
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-20 22:18:12 +0530

    Time: 2 ms (53.13%) | Memory: 7.4 MB (40.47%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -10,9 +10,8 @@ public:
         return x;
     }
     bool isSameAfterReversals(int num) {
-        int org = num;
         int rev1 = rev(num);
         int rev2 = rev(rev1);
-        return rev2 == org;
+        return rev2 == num;
     }
 };
