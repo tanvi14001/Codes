d73365b7e7b1c30f8ed5289690f945908cf2469e
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-08-31 21:26:28 +0530

    Time: 41 ms (15.43%) | Memory: 22.5 MB (75.75%) - LeetSync

---
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string,int>mpp;
+        int cnt=0;
+        for(auto c:words1)
+        {
+            if(mpp[c]==0) mpp[c]=1;
+            else if(mpp[c]==1) mpp[c]=-1;
+        }
+        for(auto c:words2)
+        {
+            if(mpp[c]==1) mpp[c]=2;
+            else if(mpp[c]==2) mpp[c]=-1;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second==2)
+            cnt++;
+        }
+        return cnt;
+    }
+};
