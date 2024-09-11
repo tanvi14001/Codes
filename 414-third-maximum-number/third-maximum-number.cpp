733eb01f20b1ca8d3dd7b0009b3bad17cf289cba
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-09-11 23:22:44 +0530

    Time: 9 ms (20.65%) | Memory: 13.5 MB (22.54%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -2,22 +2,11 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         set<int> st(nums.begin(), nums.end());
-        vector<int> ans;
+        vector<int> ans(st.begin(), st.end());
 
-        for (auto it : st)
-            ans.push_back(it);
-
-        if (ans.size() == 1)
-            return ans[0];
-
-        if (ans.size() == 2) {
-            sort(ans.begin(), ans.end());
-            return ans[1];
-        }
-
-        if (ans.size() >= 3)
+        if (ans.size() < 3)
+            return ans.back();
+        else
             return ans[ans.size() - 3];
-
-        return ans[ans.size()];
     }
 };
