4207cb15b2cab91e783fdd37a9c2e1a6887d6e7b
tanvi14001 <89631255+tanvi14001@users.noreply.github.com>
2024-10-18 12:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (15.82%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double pow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        double ans = double(pow(x, n / 2));
+        if (n % 2 == 0) {
+            double ans2 = double(ans * ans);
+            return ans2;
+        } else {
+            double ans2 = double(x * ans * ans);
+            return ans2;
+        }
+    }
+
+    double myPow(double x, int n) {
+        double ans1 = pow(x, n);
+
+        if (n < 0)
+            ans1 = 1 / ans1;
+        return ans1;
+    }
+};
